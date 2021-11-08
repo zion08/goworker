6f9a248b43b24be34b9de225d61d5f48924832e6
seohyunnie <ll8006ll@nate.com>
2021-11-08 10:39:58 +0900

    관심목록

---
+++ src/main/java/bean/FavoriteDTO.java
@@ -0,0 +1,25 @@
+package bean;
+
+public class FavoriteDTO {
+	private int smember_num;
+	private int sproject_num;
+	private String userid;
+	public int getSmember_num() {
+		return smember_num;
+	}
+	public void setSmember_num(int smember_num) {
+		this.smember_num = smember_num;
+	}
+	public int getSproject_num() {
+		return sproject_num;
+	}
+	public void setSproject_num(int sproject_num) {
+		this.sproject_num = sproject_num;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+}
