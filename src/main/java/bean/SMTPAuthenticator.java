759538d047a625042b1eeaef943d3f00af94cf53
hwny2 <hueulysse@gmail.com>
2021-11-10 14:16:18 +0900

    .

--- src/main/java/bean/SMTPAuthenticator.java
+++ src/main/java/bean/SMTPAuthenticator.java
@@ -7,10 +7,4 @@ package bean;
 	        // Gmail의 경우 @gmail.com을 제외한 아이디만 입력한다.
 	        return new PasswordAuthentication("damhee6624@gmail.com", "Fkdlswl13^");
 	    }
-	    public PasswordAuthentication getPasswordAuthentication(String email, String newPassword) {
-	    	// 임시로 만듬. 
-	    	// email = "gowork.team01@gmail.com";
-	    	// newPassword = "team1234";
-	    	return new PasswordAuthentication(email , newPassword);
-	    }
 	}
